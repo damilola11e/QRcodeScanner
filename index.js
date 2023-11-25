1ff1787690f24370712248cc78dbbea00baabe37
damilola11e <118817700+damilola11e@users.noreply.github.com>
2023-11-25 17:24:07 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// let num=5;
+// let result=num>10 ? "Good Morning kanye" : "I used to Love Kanye";
+// alert(result)
+
+// const num=[1,2,3,4,5,6]
+// const add=num.concat('damilola','doherty');
+// // console.log(add);
+// // console.log(num);
+
+// const add=[1,2,3,4,5,6,7,8,9,10];
+// const del=add.map((e)=>(
+//     e*3
+// ))
+// console.log(del)
